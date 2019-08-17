966e2f964c226373760257d4d53ee80bac607bfd
YHPeter <44126839+YHPeter@users.noreply.github.com>
2019-08-17 14:38:33 +0800

    Update https网页获取.py

--- https网页获取.py
+++ https网页获取.py
@@ -1,10 +1,11 @@
 import requests
 def getHTMLText(url):
     try:
-        x=requests.get(url,timeout=30)
-        r.raise_for_status() #，引发HTTPExror异常
-        r.encoding=r.apparent_encoding
-        return r.text
+        ssl._create_default_https_context = ssl._create_stdlib_context
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ''Chrome/51.0.2704.63 Safari/537.36'}
+        req = urllib.request.Request(url, headers=headers)
+        web_page = urllib.request.urlopen(req)
+        return web_page.read().decode('utf-8')
     except:
         return "产生异常"
 if __name__=="__main__":
