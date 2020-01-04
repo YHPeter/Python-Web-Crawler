50982aad4da7fe3b63b86e7a5eb9a0475809117a
YHPeter <44126839+YHPeter@users.noreply.github.com>
2020-01-04 11:27:29 +0800

    Update and rename https网页获取.py to get https website.py

--- get https website.py
+++ get https website.py
@@ -1,7 +1,7 @@
 import requests
 def getHTMLText(url):
     try:
-        ssl._create_default_https_context = ssl._create_stdlib_context
+        ssl._create_default_https_context = ssl._create_stdlib_context #the most important step
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ''Chrome/51.0.2704.63 Safari/537.36'}
         req = urllib.request.Request(url, headers=headers)
         web_page = urllib.request.urlopen(req)
